e936ce71feb07d005ac0fac18706a5742d2f7c6d
JaeminKim <britten714@naver.com>
2017-12-09 15:33:07 +0900

    Using override to enable OnStartHost()
    Confer my lecture note about overriding.

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager
+{
+    public void MyStartHost()
+    {
+        StartHost();
+        Debug.Log("Starting Host at " + Time.timeSinceLevelLoad);
+    }
+
+    public override void OnStartHost()      //OnStartHost() 메서드가 아마도 virtual (또는 abstract)라서 오버라이딩이 가능한 것 같다. 어떤 메서드가 virtual인지 아닌지 어떻게 파악하는 지는 모르겠다. 다만 이 OnStartHost()는 이벤트함수 같은데 정확한 작동원리는 모르겠다... host 가 start 할 때 실행된다고 하는데 그걸 어떻게 인식한거지? 아마 StartHost() 메서드 때문인 것 같긴 한데... 
+    {                                       
+        Debug.Log("Host started at " + Time.timeSinceLevelLoad);
+    }
+}
